4735e92dcb9bb33220e692692b83b1553f6dbb1b
KenleyLuk <kentyhung@gmail.com>
2025-03-31 16:54:34 -0400

    feat: init About page

---
+++ src/componets/About/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto px-4 py-16 bg-yellow-400">
+            <div className="space-y-8">
+                <h1 className="text-2xl font-bold">Hi kenley here 👋</h1>
+                
+                <div className="space-y-4">
+                    <div className="text-xl">
+                        I'm a mobile application developer with 5 years of experience 🚀
+                    </div>
+                    <div className="text-x1">
+                        Using Kotlin, Java, Dart and Swift. Reach out if you'd like to learn more!
+                    </div>
+                </div>
+
+                <div className="pt-8">
+                    <h2 className="text-2xl font-bold mb-4">Resume</h2>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 自定義顏色
+      },
+      spacing: {
+        // 自定義間距
+      },
+    },
+  },
+  plugins: [],
+}
